b6667963dffae16f553a3489df8feee4131148d6
Rfraga26031999 <rogeliofragaa@gmail.com>
2022-04-19 14:39:41 -0300

    Incorporando localStorage

--- incorporando_eventos/js/index.js
+++ incorporando_eventos/js/index.js
@@ -1,36 +1,86 @@
+// Solicitamos algunos datos para mejorar la experiencia de usuario.
+function registrarNombrePersona() {
+  let nombreRegistrado = localStorage.getItem('nombre');
+  let apellidoRegistrado = localStorage.getItem('apellido');
+
+  if (nombreRegistrado && apellidoRegistrado) {
+    alert(`Un placer volver a verte ${nombreRegistrado}`);
+  }
+  else {
+    let nombre = '';
+    let apellido = '';
+    while (nombre === '' || apellido === '') {
+      nombre = prompt("Ingrese su nombre: ");
+      apellido = prompt("Ingrese su apellido: ");
+      if (!(nombre === '' && apellido === '')) {
+        localStorage.setItem('nombre', nombre);
+        localStorage.setItem('apellido', apellido);
+        alert(`Bienvenido a la familia Welfare ${nombre}!`);
+        break;
+      }
+    }
+  }
+}
+let nombrePersona = localStorage.getItem('nombre');
+let apellidoPersona = localStorage.getItem('apellido');
+
+function renderizar() {
+  // Creamos un div contenedor para cada producto.
+  for (let producto of productosElegidos) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `
+      <h4>${producto.producto}</h4>
+      <p>Precio: $${producto.calcularPrecioDeVenta()}</p>
+      <p>Marca: ${producto.marca}</p>
+      <p>${producto.descripcion}</p>`;
+    document.body.appendChild(contenedor);
+  }
+
+  let comprar = document.createElement("button");
+  comprar.innerHTML = "Comprar";
+  comprar.setAttribute("id", "comprar");
+  document.body.appendChild(comprar);
+  comprar.onclick = () => {eleccionDeProductos()}
+}
+
+
 // Funcion de login con validacion de usuario e intentos.
 function login() {
-  let usuario = prompt("Ingrese su usuario: ");
-  let contra = prompt("Ingrese su contraseña: ");
-  let user = '';
-  let pass = '';
-  let intentos = 3;
+  let usuarioIngreso = localStorage.getItem('usuario');
+  let contraIngreso = localStorage.getItem('contra');
 
-  if (!(usuario === '' && contra === '')) {
-    alert("Por favor, ingrese nuevamente sus datos para corroborar.")
-
+  if (usuarioIngreso && contraIngreso) {
+    alert("Ya estas logueado.");
+    renderizar();
+  }
+  else {
+    alert("Usted no esta registrado.");
+    alert("Bienvenido al formulario de registro, por favor siga los pasos: ");
+    let user = '';
+    let pass = '';
+    let intentos = 3;
+    let usuario = '';
+    let contra = '';
+    while (usuario === '' || contra === '') {
+      usuario = prompt("Ingrese su usuario: ");
+      contra = prompt("Ingrese su contraseña: ");
+      if (!(usuario === '' && contra === '')) {
+        localStorage.setItem('usuario', usuario);
+        localStorage.setItem('contra', contra);
+        alert("Registro exitoso.");
+        break;
+      }
+      else {
+        alert("Vuelva a intentarlo, por favor.");
+      }
+    }
     while (intentos > 0 && (usuario !== user || contra !== pass)) {
-      user = prompt("Ingrese su usuario: ");
-      pass = prompt("Ingrese su contraseña: ");
+      alert("Iniciar sesión.");
+      user = prompt("Usuario: ");
+      pass = prompt("Contraseña: ");
       if (user === usuario && pass === contra) {
         alert("Bienvenido, acceso permitido");
-
-        // Creamos un div contenedor para cada producto.
-        for (let producto of productosElegidos) {
-          let contenedor = document.createElement("div");
-          contenedor.innerHTML = `
-            <h4>${producto.producto}</h4>
-            <p>Precio: $${producto.calcularPrecioDeVenta()}</p>
-            <p>Marca: ${producto.marca}</p>
-            <p>${producto.descripcion}</p>`;
-          document.body.appendChild(contenedor);
-        }
-
-        let comprar = document.createElement("button");
-        comprar.innerHTML = "Comprar";
-        comprar.setAttribute("id", "comprar");
-        document.body.appendChild(comprar);
-        comprar.onclick = () => {eleccionDeProductos()}
+        renderizar();
       }
 
       else {
@@ -47,12 +97,26 @@ iniciar.setAttribute("id", "iniciar");
 document.body.appendChild(iniciar);
 iniciar.onclick = () => {login()}
 
+// let logout = document.createElement("button");
+// logout.innerHTML = "Log out";
+// logout.setAttribute("id", "logout");
+// document.body.appendChild(logout);
+// logout.onclick = () => {logout()}
+
+// function logout() {
+//   localStorage.clear();
+//   location.reload();
+// }
+
 // Cambio del mensaje de bienvenida.
 let h1 = document.getElementById("bienvenida");
-h1.innerHTML = "<h2>Bienvenido a Welfare</h2>";
+h1.innerHTML = `<h2>Welfare Home</h2>`;
 setTimeout(() => {
-  h1.innerText = "Welfare: tu siguiente nivel."
-}, 3000);
+  h1.innerText = `En Welfare, estamos para ayudarte.
+
+  Por favor, inicie sesión para continuar.
+  `
+}, 2000);
 
 // Declaracion y asignacion de la variable iva, unico impuesto a tener en cuenta para la facturacion de la compra.
 let iva = 0.21;
@@ -121,17 +185,13 @@ let productosElegidos = [
   producto2
 ];
 
-// Solicitamos algunos datos para mejorar la experiencia de usuario.
-let nombre = prompt("Ingrese su nombre");
-let apellido = prompt("Ingrese su apellido");
-
 let total = 0;
 let seleccion = [];
 
 // Esta funcion nos sirve para iterar la seleccion de productos del usuario y los va sumando al array vacio de arriba, hasta que este digite NO.
 function eleccionDeProductos() {
   let eleccion = prompt(
-    `Hola ${nombre}, bienvenid@ a Welfare!
+    `Hola ${nombrePersona}, bienvenid@ a la tienda Welfare!
     Selecciona los productos que desees.
     Digite 1 para: ${producto1.producto} $${producto1.calcularPrecioDeVenta()}
     Digite 2 para: ${producto2.producto} $${producto2.calcularPrecioDeVenta()}.`
@@ -145,13 +205,13 @@ function eleccionDeProductos() {
     total += productosElegidos[eleccion - 1].calcularPrecioDeVenta(); 
 
     eleccion = prompt(
-      `${nombre}, ¿Desea continuar con su compra?
+      `${localStorage.getItem('nombre')}, ¿Desea continuar con su compra?
       En caso de que su compra haya finalizado, digite NO.
       Digite 1 para: ${producto1.producto} $${producto1.calcularPrecioDeVenta()}
       Digite 2 para: ${producto2.producto} $${producto2.calcularPrecioDeVenta()}`
     );
   }
-  console.log(`${nombre}, el total a pagar es: $${total}`);
+  console.log(`${nombrePersona}, el total a pagar es: $${total}`);
   let pago = document.createElement("button");
   pago.innerHTML = "Pagar";
   pago.setAttribute("id", "pagar");
@@ -206,16 +266,16 @@ function calcularCuota(monto, tasa, meses) {
 // Dependiendo de la opcion, nos regresara un mensaje de compra exitosa o nos dirigira a un prompt donde podremos elegir la cantidad de cuotas.
 function pagar() {
 
-  let metodoPago = parseInt(prompt(`${nombre}, el monto a pagar es: $${total}.
+  let metodoPago = parseInt(prompt(`${nombrePersona}, el monto a pagar es: $${total}.
   ¿Como deseas abonar?
   Digite 1 para Debito
   Digite 2 para Credito: `));
 
   if(metodoPago === 1) {
     let tarjeta = prompt("Ingrese el numero de su tarjeta de debito: ");
-    document.write(`${nombre}, tu pago fue procesado con exito, gracias por comprar en Welfare!<br><br>
+    document.write(`${nombrePersona}, tu pago fue procesado con exito, gracias por comprar en Welfare!<br><br>
     <b>Detalle de la compra.</b><br><br>
-    <b>Compra a nombre de:</b> ${nombre} ${apellido}<br>
+    <b>Compra a nombre de:</b> ${nombrePersona} ${apellidoPersona}<br>
     <b>Numero de tarjeta:</b> ${tarjeta}<br>
     <b>Productos seleccionados:</b> ${seleccion}<br>
     <b>Total abonado:</b> $${total}`);
@@ -224,7 +284,7 @@ function pagar() {
   else if(metodoPago === 2) {
     let tarjeta = prompt("Ingrese el numero de su tarjeta de credito: ");
     
-    let cuotas = parseInt(prompt(`${nombre}, ¿En cuantas cuotas desea realizar su pago?
+    let cuotas = parseInt(prompt(`${nombrePersona}, ¿En cuantas cuotas desea realizar su pago?
     Digite 1 para 3 cuotas con un recargo del % ${convertirPorcentaje(calcularInteres(3))} (cuotas de $${calcularCuota(total, calcularInteres(3), 3)})
     Digite 2 para 6 cuotas con un recargo del % ${convertirPorcentaje(calcularInteres(6))} (cuotas de $${calcularCuota(total, calcularInteres(3), 6)})
     Digite 3 para 12 cuotas con un recargo del % ${convertirPorcentaje(calcularInteres(12))} (cuotas de $${calcularCuota(total, calcularInteres(3), 12)})`));
@@ -237,7 +297,7 @@ function pagar() {
         total += recargo;
         document.write(`${nombre}, tu pago fue exitoso, gracias por comprar en Welfare!<br><br>
         <b>Detalles de la compra:</b><br><br>
-        <b>Compra a nombre de:</b> ${nombre} ${apellido}<br>
+        <b>Compra a nombre de:</b> ${nombrePersona} ${apellidoPersona}<br>
         <b>Numero de tarjeta:</b> ${tarjeta}<br>
         <b>Productos seleccionados:</b> ${seleccion}<br>
         <b>Total abonado:</b> $${total.toFixed(2)}`);
@@ -246,9 +306,9 @@ function pagar() {
       case 2:
         recargo = total * calcularInteres(6);
         total += recargo;
-        document.write(`${nombre}, tu pago fue exitoso, gracias por comprar en Welfare!<br><br>
+        document.write(`${nombrePersona}, tu pago fue exitoso, gracias por comprar en Welfare!<br><br>
         <b>Detalles de la compra:</b><br><br>
-        <b>Compra a nombre de:</b> ${nombre} ${apellido}<br>
+        <b>Compra a nombre de:</b> ${nombrePersona} ${apellidoPersona}<br>
         <b>Numero de tarjeta:</b> ${tarjeta}<br>
         <b>Productos seleccionados:</b> ${seleccion}<br>
         <b>Total abonado:</b> $${total.toFixed(2)}`);
@@ -257,9 +317,9 @@ function pagar() {
       case 3:
         recargo = total * calcularInteres(12);
         total += recargo;
-        document.write(`${nombre}, tu pago fue exitoso, gracias por comprar en Welfare!
+        document.write(`${nombrePersona}, tu pago fue exitoso, gracias por comprar en Welfare!
         <b>Detalles de la compra:</b><br><br>
-        <b>Compra a nombre de:</b> ${nombre} ${apellido}<br>
+        <b>Compra a nombre de:</b> ${nombrePersona} ${apellidoPersona}<br>
         <b>Numero de tarjeta:</b> ${tarjeta}<br>
         <b>Productos seleccionados:</b> ${seleccion}<br>
         <b>Total abonado:</b> $${total.toFixed(2)}`);
